0573e0e618cfd8a41e4f00b10c0fcb8611929fad
Clare Forrest <clare.a.forrest@gmail.com>
2021-01-27 17:10:53 +1100

    created styled components for main page

---
+++ src/styled/main.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const HeadingMain = styled.h1`
+  font-family: Arial, sans-serif;
+  font-size: 24px;
+  color: black;
+`
+
+export const HeadingSub = styled.h2`
+  font-family: Arial, sans-serif;
+  font-size: 20px;
+  color: black;
+`
+
+export const Page = styled.div`
+  width: 100%;
+  margin-left: 25%;
+  height: 100vh;
+  back-ground-color: yellow;
+  color: black;
+`
+
+export const Nav = styled.nav`
+  width: 100%;
+  height: 45px;
+  background-color: black;
+  color: white;
+`
+
+export const SidePanel = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: rgb(191, 243, 191);
+`
+
+export const Card = styled.div`
+  border: solid, black, 1px;
+  height:  100%;
+  width: 100%;
+`
