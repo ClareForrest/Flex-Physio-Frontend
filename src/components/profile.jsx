a735991e92eb8c6cb8f3651f6b41d5262c498253
Clare Forrest <clare.a.forrest@gmail.com>
2021-01-28 16:48:28 +1100

    set up bootstrap modal in profile

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,44 +1,53 @@
 import { HeadingMain } from '../styled/main';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import React, {useState} from 'react';
+
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Your contact/address details (change this with state)
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>
+          Need the edit form to show up here (again, change with state)
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 
 export function Profile() {
-  return(
+  const [modalShow, setModalShow] = React.useState(false);
+
+  return (
     <>
-    <div>
-      <div class="row justify-content-center">
+      <Row>
         <HeadingMain>Your Contact Details:</HeadingMain>
-      </div>
-      <div class="row justify-content-center">
-        <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#exampleModalCenter">
-          Edit
-        </button>
-      </div>
-      <div class="row justify-content-center">
+      </Row>
+      <Button variant="secondary" onClick={() => setModalShow(true)}>
+        Edit
+      </Button>
+      <MyVerticallyCenteredModal show={modalShow} onHide={() => setModalShow(false)}/>
+      <Row>
         <HeadingMain>Your Address Details:</HeadingMain>
-      </div>
-      <div class="row justify-content-center">
-        <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#exampleModalCenter">
-          Edit
-        </button>
-      </div>
-      </div>
-      <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLongTitle">Your Current Details:</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            /*use state to put in the details of either personal or address */
-          </div>
-         <div class="modal-footer">
-           <button type="button" class="btn btn-secondary">Save changes</button>
-         </div>
-       </div>
-     </div>
- </div>
+      </Row>
+      <Button variant="secondary" onClick={() => setModalShow(true)}>
+        Edit
+      </Button>
+      <MyVerticallyCenteredModal show={modalShow} onHide={() => setModalShow(false)}/>
     </>
   )
-  }
+}
