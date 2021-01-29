9d15081c476058dea7ce95d68ac07f9bf3797e3d
Clare Forrest <clare.a.forrest@gmail.com>
2021-01-29 15:15:05 +1100

    fixed routing error with app.jsx and index.js

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Template } from './components/Template';
+import { NavRouter } from './components/NavRouter';
+
+
+function App() {
+
+  return (
+    <>
+      <Template>
+        <NavRouter></NavRouter>
+      </Template>
+    </>
+  );
+}
+
+export default App;
