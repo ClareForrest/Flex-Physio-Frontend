05398b4cd14ea8b0f739e4601e53a508872dcbf3
Chris Scott <cam022007@coderacademy.edu.au>
2021-01-30 07:13:00 -0800

    can now render an empty array in console from rails server which will eventually be filled by a user employee's availability

--- src/components/Availability.jsx
+++ src/components/Availability.jsx
@@ -1,6 +1,29 @@
-import React, { useState}  from 'react';
+import React, { useState, useEffect }  from 'react';
 
+export function Availability() {
+  const [employee, setEmployee] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:3000/api/availabilities')
+    .then((response) => response.json())
+    .then((body) => {
+      console.log(body);
+      setEmployee(body)
+    });
+  }, []);
 
-export function availability(item) {
-  
+  return(
+    <>
+    <h1>Schedule</h1>
+    <div>
+      {employee && employee.map((s) => {
+        return (
+          <p key={s.id}>
+            <h2>{s.availability}</h2>
+          </p>
+        );
+      })}
+    </div>
+    </> 
+
+  )
 }
--- src/components/NavRouter.jsx
+++ src/components/NavRouter.jsx
@@ -13,6 +13,7 @@ import { Profile } from './Profile';
 import { Bookings } from './Bookings'; 
 import { Login } from './Login';
 import { SignUp } from './SignUp';
+import { Availability } from './Availability';
 
 
 export function NavRouter() {
@@ -32,8 +33,9 @@ export function NavRouter() {
         <Route exact path="/bookings" component={Bookings} />
         <Route exact path="/contact-us" component={HomePage} />
         <Route exact path="/profile" component={Profile} />
-        <Route exact path="/login" component={Login} /> {/* I want to change this dynamically to be either login/sign out dependant on state */}
+        <Route exact path="/login" component={Login} /> 
         <Route exact path="/sign-up" component={SignUp} />
+        <Route exact path="/availabilities" component={Availability} />
         <Route exact path="/homepageloggedin" component={HomePageLoggedIn} /> {/*I want to have this link on the icon. That way you can still get to home/services/about us etc through home, but can also see your logged in home page*/}
       </Switch>
     </BrowserRouter>
