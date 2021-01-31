b773478ef5569344c05e66821ca7368762eec4d9
Chris Scott <cam022007@coderacademy.edu.au>
2021-01-31 15:49:11 -0800

    resolved conflicts

--- src/components/NavRouter.jsx
+++ src/components/NavRouter.jsx
@@ -28,25 +28,12 @@ export function NavRouter() {
   }
 
   return (
-<<<<<<< HEAD
-    <>
-      <Nav>
-        <Link to="/">Home</Link>   
-        <Link to="/about">About</Link>   
-        <Link to="/bookings">Bookings</Link>
-        <Link to="/contact-us">Contact Us</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/" onClick={logout}>Logout</Link>
-      </Nav>
-      
-=======
     <BrowserRouter>
       if (auth){
         <LoggedInNav />
       } else {
         <LoggedOutNav />
       }
->>>>>>> 6cfa8229e2b32a67b1dd9225c114f4c3009e9a63
 
       <Switch>
         <Route exact path="/" component={HomePage} />
