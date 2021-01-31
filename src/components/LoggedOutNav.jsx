042042123b805b8414b15522ff902bfb7794d8ab
Clare Forrest <clare.a.forrest@gmail.com>
2021-02-01 10:28:39 +1100

    updated loggedout nav

--- src/components/LoggedOutNav.jsx
+++ src/components/LoggedOutNav.jsx
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
 import React from "react";
 import { useHistory, Link } from "react-router-dom";
-
-
-export function LoggedOutNavbar() {
-  return (
-    <nav>
-      <Link to="/login">Login</Link>
-      <Link to="/sign-up">Sign Up</Link>
-    </nav>
-=======
 import React, { Link, Nav } from "react";
 import { BrowserRouter } from "react-router-dom";
-
 
 export function LoggedOutNav() {
   return (
@@ -24,6 +13,5 @@ export function LoggedOutNav() {
       <Link to="/login">Login</Link>
     </Nav>
     </BrowserRouter>
->>>>>>> 6cfa8229e2b32a67b1dd9225c114f4c3009e9a63
   );
 }
