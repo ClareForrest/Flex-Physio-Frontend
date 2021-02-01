64ab4799fcbb99417adbc033798a0ecc54ac65aa
Chris Scott <cam022007@coderacademy.edu.au>
2021-01-31 16:50:27 -0800

    re-setting up to show individual employee availability

---
+++ src/components/IndividualAvailability.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect }  from 'react';
+
+
+export function IndividualAvailability(props) {
+  const [employee, setEmployee] = useState(null);
+  const id = props.match.params.id;
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/api/availability/${id}`)
+      .then((res) => res.json())
+      .then((employee) => {
+        console.log(id)
+        console.log(employee)
+        setEmployee(employee);
+      });
+  }, [id])
+
+  return (
+    employee && (
+      <>
+        <div>
+          <h2>testing</h2>
+        </div>
+      </>
+    )
+  )
