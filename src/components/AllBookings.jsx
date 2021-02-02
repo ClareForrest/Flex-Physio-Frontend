6d418b38b3e755585257c2f8407011bee970c229
Clare Forrest <clare.a.forrest@gmail.com>
2021-02-03 10:22:44 +1100

    ammended allbookings to allbookingsfunction and corrected code to display values from object

--- src/components/AllBookings.jsx
+++ src/components/AllBookings.jsx
@@ -1,34 +1,36 @@
-import { HeadingMain, HeadingSub } from '../styled/main';
+import { HeadingMain } from '../styled/main';
 import React, { useState, useEffect } from "react";
 import Card from 'react-bootstrap/Card';
-  
-  export function AllBookings(){
+
+// i want to access each item in the Array. the items are objects. 
+// once i have an individual object, 
+// then i want to access the values from its keys
+  export function AllBookingsFunction(){
     const [allBookings, setAllBookings] = useState([])
     useEffect(() => {
       fetch('http://localhost:3000/api/bookings/history')
       .then((response) => response.json())
-      .then((body) => {
-        console.log(body)
-        // needs to show all bookings
-        setAllBookings(body)
-        // needs to show the most recently saved booking. Would that be .last??
-        // setCurrentBooking(body)
-      });
+      .then((body) => setAllBookings(body))
     }, []);
 
-    
-  return (
-    <>
-      <HeadingMain>Your Previous Bookings</HeadingMain>
-        <div>
-          {allBookings && allBookings.map((booking) => {
-            return (
-              <Card key={booking.id}>
-                {booking.id}
-              </Card>
-            )
-          })}
-        </div>
-    </>
-  );
-}
+    console.log(allBookings)
+    return (
+      <>
+        <HeadingMain>Your Previous Bookings</HeadingMain>
+            {allBookings.map((booking, index) => {
+              return (
+                <div key={index}>
+                  <Card class="booking-cards">
+                    <p>Location: {booking.location}</p>
+                    <p>Date: {booking.date}</p>
+                    <p>Time: {booking.time}</p>
+                    <p>Service: {booking.service}</p>
+                    <p>Cost: ${booking.cost}</p>
+                  </Card>
+                </div>
+              )
+            }
+          )}
+      </>
+    );
+  }
