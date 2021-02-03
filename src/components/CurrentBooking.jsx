b7a8487aae8ab6f1e8ab4c356b4c23f6d7149a0c
Clare Forrest <clare.a.forrest@gmail.com>
2021-02-03 13:01:28 +1100

    removed excess lines and unnecessary comments. Set up display config for current booking.

--- src/components/CurrentBooking.jsx
+++ src/components/CurrentBooking.jsx
@@ -5,22 +5,29 @@ import Card from 'react-bootstrap/Card';
   export function CurrentBooking(){
     const [currentBooking, setCurrentBooking] = useState([])
     useEffect(() => {
-      fetch('http://localhost:3000/api/bookings/current/:id')
+      fetch('http://localhost:3000/api/bookings/current')
       .then((response) => response.json())
-      .then((body) => {
-        // needs to show the most recently saved booking. Would that be .last??
-        console.log(body)
-        setCurrentBooking(body.last)
-      });
+      .then((body) => setCurrentBooking(body))
     }, []);
+    // shows the most recently saved booking in the array
 
     return (
       <>
         <HeadingMain>Your Current Booking</HeadingMain>
-        <Card>
-
-
-        </Card>
+            {currentBooking.map((booking, index) => {
+              return (
+                <div key={index}>
+                  <Card class="booking-cards">
+                    <p>Location: {booking.location}</p>
+                    <p>Date: {booking.date}</p>
+                    <p>Time: {booking.time}</p>
+                    <p>Service: {booking.service}</p>
+                    <p>Cost: ${booking.cost}</p>
+                  </Card>
+                </div>
+              )
+            }
+          )}
     </>
   );
 }
