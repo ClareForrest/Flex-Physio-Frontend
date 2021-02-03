de46e6f4dfb02c80c6ac2cd6dd1072a4414746e4
Chris Scott <cam022007@coderacademy.edu.au>
2021-02-03 15:40:10 -0800

    fixed file paths

--- src/components/UpdateProfile.jsx
+++ src/components/UpdateProfile.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import { HeadingMain } from './styled/HeadingMain';
+import { HeadingMain } from '../styled/main';
 // import { useParams } from "react-router-dom";
 
 export function UpdateProfile() {
