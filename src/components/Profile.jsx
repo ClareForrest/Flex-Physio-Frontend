0516c9d24e6d1fa60db4d33548f404ecf61dc197
Clare Forrest <clare.a.forrest@gmail.com>
2021-02-06 12:59:20 +1100

    updated profile component to show user details and address details

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,15 +1,16 @@
 import { HeadingMain } from "../styled/main";
-import Modal from "react-bootstrap/Modal";
+// import Modal from "react-bootstrap/Modal";
 import { Card, Button } from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 import React, { useState, useEffect } from "react";
+import { UpdateProfile } from "./UpdateProfile";
 
-function GetProfile(props) {
+export function GetProfile(props) {
   const [address, setAddress] = useState(null);
   const user = JSON.parse(localStorage.getItem("user"));
   const id = user.id;
 
-  // check where and how this is pulling through. currently returnign undefined.
+  // check where and how this is pulling through. currently returning undefined.
   useEffect(() => {
     fetch(`${process.env.REACT_APP_BACKEND_URL}/api/addresses/${id}`)
       .then((res) => res.json())
@@ -21,21 +22,26 @@ function GetProfile(props) {
     address && (
       <>
         <Row>
-          <HeadingMain>Your Current Address</HeadingMain>
+          <HeadingMain>Your Contact Details</HeadingMain>
         </Row>
         <Row>
           <Card className="login-card-size">
-            <ul>
-              <li>{address.street}</li>
-              <li>{address.suburb}</li>
-              <li>{address.state}</li>
-              <li>{address.postcode}</li>
-            </ul>
-            <Button
-              variant="secondary"
-              type="button"
-              onClick={MyVerticallyCenteredModal}
-            >
+            <p>First Name: {user.first_name}</p>
+            <p>Last Name: {user.last_name}</p>
+            <p>Email: {user.email}</p>
+            <p>Phone number: {user.phone_number}</p>
+          </Card>
+        </Row>
+        <Row>
+          <HeadingMain>Your Address Details</HeadingMain>
+        </Row>
+        <Row>
+          <Card className="login-card-size">
+            <p>Street: {address.street}</p>
+            <p>Suburb: {address.suburb}</p>
+            <p>State: {address.state}</p>
+            <p>Postcode: {address.postcode}</p>
+            <Button variant="secondary" type="button" onClick={UpdateProfile}>
               Edit
             </Button>
           </Card>
@@ -45,59 +51,62 @@ function GetProfile(props) {
   );
 }
 
-function MyVerticallyCenteredModal(props) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Your contact/address details (change this with state)
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <p>Need the edit form to show up here (again, change with state)</p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-}
+// Don't think we need the below if we are redirecting and rendering the UpdateProfile component.
+// Ultimately, I would like to use
 
-export function Profile() {
-  const [modalShow, setModalShow] = useState(false);
-  const user = JSON.parse(localStorage.getItem("user"));
-  return (
-    <>
-      <Row>
-        <HeadingMain>Your Contact Details:</HeadingMain>
-      </Row>
-      <h6>First Name: {user.first_name}</h6>
-      <h6>Last Name: {user.last_name}</h6>
-      <h6>Current Email: {user.email}</h6>
-      <h6>Current contact number: {user.phone_number}</h6>
-      <Button variant="secondary" onClick={() => setModalShow(true)}>
-        Edit
-      </Button>
-      <MyVerticallyCenteredModal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-      />
-      <Row>
-        <HeadingMain>Your Address Details:</HeadingMain>
-        <GetProfile></GetProfile>
-      </Row>
-      <Button variant="secondary" onClick={() => setModalShow(true)}>
-        Edit
-      </Button>
-      <MyVerticallyCenteredModal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-      />
-    </>
-  );
-}
+// function MyVerticallyCenteredModal(props) {
+//   return (
+//     <Modal
+//       {...props}
+//       size="lg"
+//       aria-labelledby="contained-modal-title-vcenter"
+//       centered
+//     >
+//       <Modal.Header closeButton>
+//         <Modal.Title id="contained-modal-title-vcenter">
+//           Your contact/address details (change this with state)
+//         </Modal.Title>
+//       </Modal.Header>
+//       <Modal.Body>
+//         <p>Need the edit form to show up here (again, change with state)</p>
+//       </Modal.Body>
+//       <Modal.Footer>
+//         <Button onClick={props.onHide}>Close</Button>
+//       </Modal.Footer>
+//     </Modal>
+//   );
+// }
+
+// export function Profile() {
+//   const [modalShow, setModalShow] = useState(false);
+//   const user = JSON.parse(localStorage.getItem("user"));
+//   return (
+//     <>
+//       <Row>
+//         <HeadingMain>Your Contact Details:</HeadingMain>
+//       </Row>
+//       <h6>First Name: {user.first_name}</h6>
+//       <h6>Last Name: {user.last_name}</h6>
+//       <h6>Current Email: {user.email}</h6>
+//       <h6>Current contact number: {user.phone_number}</h6>
+//       <Button variant="secondary" onClick={() => setModalShow(true)}>
+//         Edit
+//       </Button>
+//       <MyVerticallyCenteredModal
+//         show={modalShow}
+//         onHide={() => setModalShow(false)}
+//       />
+//       <Row>
+//         <HeadingMain>Your Address Details:</HeadingMain>
+//         <GetProfile></GetProfile>
+//       </Row>
+//       <Button variant="secondary" onClick={() => setModalShow(true)}>
+//         Edit
+//       </Button>
+//       <MyVerticallyCenteredModal
+//         show={modalShow}
+//         onHide={() => setModalShow(false)}
+//       />
+//     </>
+//   );
+// }
