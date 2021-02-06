2cb35fcdefa5a95ca3389097973034acb75352e1
Clare Forrest <clare.a.forrest@gmail.com>
2021-02-06 12:22:46 +1100

    added images to carousel

--- src/components/homepageComponents/Carousel.jsx
+++ src/components/homepageComponents/Carousel.jsx
@@ -1,41 +1,43 @@
-import Carousel from 'react-bootstrap/Carousel';
+import { Image, Carousel } from "react-bootstrap";
 
-export function ImageDisplay(){
+export function ImageDisplay() {
   return (
-  <Carousel>
-    <Carousel.Item interval={1000}>
-      <img
-        className="d-block w-100"
-        src="holder.js/800x400?text=First slide&bg=373940"
-        alt="First slide"
-      />
-      <Carousel.Caption>
-        <h3>Physiotherapy</h3>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item interval={1000}>
-      <img
-        className="d-block w-100"
-        src="holder.js/800x400?text=Second slide&bg=282c34"
-        alt="second slide"
-      />
-      <Carousel.Caption>
-        <h3>Remedial Massage</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="holder.js/800x400?text=Third slide&bg=20232a"
-        alt="Third slide"
-      />
-      <Carousel.Caption>
-        <h3>Sports Massage</h3>
-        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-  </Carousel>
-  )
+    <Carousel>
+      <Carousel.Item interval={1000}>
+        <Image
+          className="d-block w-100"
+          src="../steps.jpg"
+          alt="Steps"
+        />
+        <Carousel.Caption>
+          <h3>Physiotherapy</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={1000}>
+        <img
+          className="d-block w-100"
+          src="crunches.jpg/800x400 text=Crunches slide&bg=282c34"
+          alt="Crunches"
+        />
+        <Carousel.Caption>
+          <h3>Remedial Massage</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="silhouette.jpg/800x400 text=Silhouette slide&bg=20232a"
+          alt="Silhouette"
+        />
+        <Carousel.Caption>
+          <h3>Sports Massage</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 }
