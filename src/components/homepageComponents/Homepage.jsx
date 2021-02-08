e8795ed6bfc9a8529de370bde75bcb20bcd1d317
Clare Forrest <clare.a.forrest@gmail.com>
2021-02-08 14:46:25 +1100

    redid the styling of the home,about and service components to make for better display

--- src/components/homepageComponents/Homepage.jsx
+++ src/components/homepageComponents/Homepage.jsx
@@ -1,21 +1,20 @@
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Image } from "react-bootstrap";
 import { HeadingMain, HeadingSub } from "../../styled/main";
 import massage from "../../massage.jpg";
 import physios from "../../physios.jpg";
 import admin from "../../admin.jpg";
+import silhouette from "../../silhouette.jpg";
 import "../../styled/style.css";
-import { ImageDisplay } from "./Carousel";
 
 export function HomePage() {
   return (
     <div>
-      <ImageDisplay />
+      <Image src={silhouette} className="silhouette-image" />
       <Row>
         <HeadingMain>About</HeadingMain>
       </Row>
       <Row>
         <Col>
-          <img src={physios} alt="Physiotherapists" />
           <HeadingSub id="massage-therapists" name="massage-therapists">
             Our Massage Therapists
           </HeadingSub>
@@ -28,38 +27,11 @@ export function HomePage() {
             Vulputate enim nulla aliquet porttitor lacus. Sed cras ornare arcu
             dui vivamus arcu felis.{" "}
           </p>
-          <img src={admin} alt="Administration Staff" />
-          <HeadingMain>Services</HeadingMain>
-          <HeadingSub id="massage" name="massage">
-            Massage
-          </HeadingSub>
-          <p class="set-size">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Neque
-            convallis a cras semper auctor neque. Lorem sed risus ultricies
-            tristique nulla. Pellentesque nec nam aliquam sem. Magnis dis
-            parturient montes nascetur ridiculus mus mauris vitae ultricies.
-            Vulputate enim nulla aliquet porttitor lacus. Sed cras ornare arcu
-            dui vivamus arcu felis.
-          </p>
-          <img src={physios} alt="Physiotherapy" />
-          <HeadingMain id="consult fees" name="consult fees">
-            Consult Fees
-          </HeadingMain>
-          <HeadingSub>Physiotherapy - Initial Consultation:</HeadingSub>
-          <div class="set-size">
-            <p>
-              Due to the nature of an initial consult, fees are slightly higher:
-            </p>
-            <p>$125.00</p>
-          </div>
-          <HeadingSub>Physiotherapy - Subsequent Consultations:</HeadingSub>
-          <div class="set-size">
-            <p>Subsquent consultation fees:</p>
-            <p>$95.00</p>
-          </div>
-          <HeadingMain>Contact Us</HeadingMain>
         </Col>
+        <Image src={massage} alt="Massage Therapists" />
+      </Row>
+      <Row>
+        <Image src={physios} alt="Physiotherapists" />
         <Col>
           <HeadingSub id="physiotherapists" name="physiotherapists">
             Our Physiotherapists
@@ -73,7 +45,10 @@ export function HomePage() {
             Vulputate enim nulla aliquet porttitor lacus. Sed cras ornare arcu
             dui vivamus arcu felis.
           </p>
-          <img src={massage} alt="Massage Therapists" />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
           <HeadingSub id="admin-staff" name="admin-staff">
             Our Administration Staff
           </HeadingSub>
@@ -86,7 +61,32 @@ export function HomePage() {
             Vulputate enim nulla aliquet porttitor lacus. Sed cras ornare arcu
             dui vivamus arcu felis
           </p>
-          <img src={massage} alt="Massage" />
+        </Col>
+        <Image src={admin} alt="Administration Staff" />
+      </Row>
+      <Row>
+        <HeadingMain>Services</HeadingMain>
+      </Row>
+      <Row>
+        <Col>
+          <HeadingSub id="massage" name="massage">
+            Massage
+          </HeadingSub>
+          <p class="set-size">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Neque
+            convallis a cras semper auctor neque. Lorem sed risus ultricies
+            tristique nulla. Pellentesque nec nam aliquam sem. Magnis dis
+            parturient montes nascetur ridiculus mus mauris vitae ultricies.
+            Vulputate enim nulla aliquet porttitor lacus. Sed cras ornare arcu
+            dui vivamus arcu felis.
+          </p>
+        </Col>
+        <Image src={massage} alt="Massage" />
+      </Row>
+      <Row>
+        <Image src={physios} alt="Physiotherapy" />
+        <Col>
           <HeadingSub id="physiotherapy" name="physiotherapy">
             Physiotherapy
           </HeadingSub>
@@ -99,11 +99,65 @@ export function HomePage() {
             Vulputate enim nulla aliquet porttitor lacus. Sed cras ornare arcu
             dui vivamus arcu felis.
           </p>
+        </Col>
+      </Row>
+      <Row>
+        <HeadingMain id="consult fees" name="consult fees">
+          Consult Fees
+        </HeadingMain>
+      </Row>
+      <Row>
+        <HeadingMain id="massage" name="massage">
+          Massage
+        </HeadingMain>
+      </Row>
+      <Row>
+        <Col>
           <HeadingSub>Remedial Massage:</HeadingSub>
-          <p class="set-size">$75.00 for 1 hour</p>
+        </Col>
+        <Col>
           <HeadingSub>Sports Massage:</HeadingSub>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <p class="set-size">$75.00 for 1 hour</p>
+        </Col>
+        <Col>
           <p class="set-size">$85.00 for 1 hour</p>
         </Col>
+      </Row>
+      <Row>
+        <HeadingMain id="physiotherapy" name="physiotherapy">
+          Physiotherapy
+        </HeadingMain>
+      </Row>
+      <Row>
+        <Col>
+          <HeadingSub>Initial Consultation:</HeadingSub>
+        </Col>
+        <Col>
+          <HeadingSub>Subsequent Consultations:</HeadingSub>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div class="set-size">
+            <p>
+              Due to the nature of an initial consult, fees are slightly higher:
+            </p>
+            <p>$125.00</p>
+          </div>
+        </Col>
+        <Col>
+          <div class="set-size">
+            <p>Subsquent consultation fees:</p>
+            <p>$95.00</p>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <HeadingMain>Contact Us</HeadingMain>
       </Row>
     </div>
   );
--- src/components/homepageComponents/Services.jsx
+++ src/components/homepageComponents/Services.jsx
@@ -1,6 +1,7 @@
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Image } from "react-bootstrap";
 import { HeadingMain, HeadingSub } from "../../styled/main";
 import physios from "../../physios.jpg";
+import massage from "../../massage.jpg";
 import "../../styled/style.css";
 
 export function ServicesPage() {
@@ -23,28 +24,15 @@ export function ServicesPage() {
             Vulputate enim nulla aliquet porttitor lacus. Sed cras ornare arcu
             dui vivamus arcu felis.
           </p>
-          <img src={physios} alt="Physiotherapy" />
-          <HeadingMain id="consult fees" name="consult fees">
-            Consult Fees
-          </HeadingMain>
-          <HeadingSub>Physiotherapy - Initial Consultation:</HeadingSub>
-          <div class="set-size">
-            <p>
-              Due to the nature of an initial consult, fees are slightly higher:
-            </p>
-            <p>$125.00</p>
-          </div>
-          <HeadingSub>Physiotherapy - Subsequent Consultations:</HeadingSub>
-          <div class="set-size">
-            <p>Subsquent consultation fees:</p>
-            <p>$95.00</p>
-          </div>
-          <HeadingMain>Contact Us</HeadingMain>
         </Col>
+        <Image src={massage} alt="Massage" />
+      </Row>
+      <Row>
+        <Image src={physios} alt="Physiotherapy" />
         <Col>
-          <HeadingSub id="physiotherapy" name="physiotherapy">
-            Physiotherapy
-          </HeadingSub>
+        <HeadingSub id="physiotherapy" name="physiotherapy">
+          Physiotherapy
+        </HeadingSub>
           <p class="set-size">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Neque
@@ -54,11 +42,65 @@ export function ServicesPage() {
             Vulputate enim nulla aliquet porttitor lacus. Sed cras ornare arcu
             dui vivamus arcu felis.
           </p>
+        </Col>
+      </Row>
+      <Row>
+        <HeadingMain id="consult fees" name="consult fees">
+          Consult Fees
+        </HeadingMain>
+      </Row>
+      <Row>
+          <HeadingMain id="massage" name="massage">
+            Massage
+          </HeadingMain>
+      </Row>
+      <Row>
+        <Col>
           <HeadingSub>Remedial Massage:</HeadingSub>
-          <p class="set-size">$75.00 for 1 hour</p>
+        </Col>
+        <Col>
           <HeadingSub>Sports Massage:</HeadingSub>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <p class="set-size">$75.00 for 1 hour</p>
+        </Col>
+        <Col>
           <p class="set-size">$85.00 for 1 hour</p>
         </Col>
+      </Row>
+      <Row>
+          <HeadingMain id="physiotherapy" name="physiotherapy">
+            Physiotherapy
+          </HeadingMain>
+      </Row>
+      <Row>
+        <Col>
+          <HeadingSub>Initial Consultation:</HeadingSub>
+        </Col>
+        <Col>
+          <HeadingSub>Subsequent Consultations:</HeadingSub>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div class="set-size">
+            <p>
+              Due to the nature of an initial consult, fees are slightly higher:
+            </p>
+            <p>$125.00</p>
+          </div>
+        </Col>
+        <Col>
+          <div class="set-size">
+            <p>Subsquent consultation fees:</p>
+            <p>$95.00</p>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <HeadingMain>Contact Us</HeadingMain>
       </Row>
     </div>
   );
