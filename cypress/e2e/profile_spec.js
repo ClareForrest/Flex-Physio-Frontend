f853758739785de115cfd5ba038a2e45665f91a9
Clare Forrest <clare.a.forrest@gmail.com>
2021-02-09 17:46:42 +1100

    updated and made all tests work. deleted those that didn't

--- cypress/e2e/profile_spec.js
+++ cypress/e2e/profile_spec.js
@@ -1,11 +1,21 @@
-describe('The Profile Page', () => {
+describe('logs in', () => {
+  it('using UI', () => {
+    cy.visit('/login')
+    cy.location('pathname').should('equal', '/login')
+
+    // enter valid username and password
+    cy.get('[name=email]').type('olivia@hotmail.com')
+    cy.get('[name=password]').type('123456')
+    cy.contains('button', 'Login').click()
+    cy.location('pathname').should('equal', '/')
+  })
+
+
   it('successfully loads', () => {
     cy.visit("http://localhost:8080/profile") 
   })
 
   it("should go to the profile page", () => {
-    cy.visit("/");
-    cy.findByText(/profile/).click();
-    cy.url().should("include", "/profile");
+    cy.visit("/profile");
   });
 })
